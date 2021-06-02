0de3c963d3dfaad574615e17e655ea8deb71bab3
Pedro Neto <13pneto@gmail.com>
2021-06-01 21:59:21 -0300

    DataBinding-Event

--- Blazor_WebAssembly1/obj/Debug/net5.0/RazorDeclaration/Pages/DataBindingEvent.razor.g.cs
+++ Blazor_WebAssembly1/obj/Debug/net5.0/RazorDeclaration/Pages/DataBindingEvent.razor.g.cs
@@ -4,7 +4,7 @@
 #pragma warning disable 0649
 #pragma warning disable 0169
 
-namespace Blazor_WebAssembly1.Shared
+namespace Blazor_WebAssembly1.Pages
 {
     #line hidden
     using System;
@@ -89,7 +89,8 @@ using Blazor_WebAssembly1.Entities;
 #line default
 #line hidden
 #nullable disable
-    public partial class NavMenu : Microsoft.AspNetCore.Components.ComponentBase
+    [Microsoft.AspNetCore.Components.RouteAttribute("/databinding-event")]
+    public partial class DataBindingEvent : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
@@ -97,16 +98,9 @@ using Blazor_WebAssembly1.Entities;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 43 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\Shared\NavMenu.razor"
+#line 16 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\Pages\DataBindingEvent.razor"
        
-    private bool collapseNavMenu = true;
-
-    private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
-
-    private void ToggleNavMenu()
-    {
-        collapseNavMenu = !collapseNavMenu;
-    }
+    public string Mensagem { get; set; } = "Mensagem";
 
 #line default
 #line hidden
