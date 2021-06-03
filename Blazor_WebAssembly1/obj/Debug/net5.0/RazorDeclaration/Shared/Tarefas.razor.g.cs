8d62d27a2cbc42cc7f1ae01890efe834e55e8ba4
Pedro Neto <13pneto@gmail.com>
2021-06-03 19:13:51 -0300

    organized order components

--- Blazor_WebAssembly1/obj/Debug/net5.0/RazorDeclaration/Shared/Tarefas.razor.g.cs
+++ Blazor_WebAssembly1/obj/Debug/net5.0/RazorDeclaration/Shared/Tarefas.razor.g.cs
@@ -13,77 +13,77 @@ namespace Blazor_WebAssembly1.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\_Imports.razor"
+#line 1 "C:\Users\User\Documents\Projetos\Blazor-WASM\Blazor_WebAssembly1\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\_Imports.razor"
+#line 2 "C:\Users\User\Documents\Projetos\Blazor-WASM\Blazor_WebAssembly1\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\_Imports.razor"
+#line 3 "C:\Users\User\Documents\Projetos\Blazor-WASM\Blazor_WebAssembly1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\_Imports.razor"
+#line 4 "C:\Users\User\Documents\Projetos\Blazor-WASM\Blazor_WebAssembly1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\_Imports.razor"
+#line 5 "C:\Users\User\Documents\Projetos\Blazor-WASM\Blazor_WebAssembly1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\_Imports.razor"
+#line 6 "C:\Users\User\Documents\Projetos\Blazor-WASM\Blazor_WebAssembly1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\_Imports.razor"
+#line 7 "C:\Users\User\Documents\Projetos\Blazor-WASM\Blazor_WebAssembly1\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\_Imports.razor"
+#line 8 "C:\Users\User\Documents\Projetos\Blazor-WASM\Blazor_WebAssembly1\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\_Imports.razor"
+#line 9 "C:\Users\User\Documents\Projetos\Blazor-WASM\Blazor_WebAssembly1\_Imports.razor"
 using Blazor_WebAssembly1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\_Imports.razor"
+#line 10 "C:\Users\User\Documents\Projetos\Blazor-WASM\Blazor_WebAssembly1\_Imports.razor"
 using Blazor_WebAssembly1.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\_Imports.razor"
+#line 14 "C:\Users\User\Documents\Projetos\Blazor-WASM\Blazor_WebAssembly1\_Imports.razor"
 using Blazor_WebAssembly1.Entities;
 
 #line default
@@ -97,7 +97,7 @@ using Blazor_WebAssembly1.Entities;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 70 "C:\Users\Pedro\source\repos\Blazor_WebAssembly1\Blazor_WebAssembly1\Shared\Tarefas.razor"
+#line 70 "C:\Users\User\Documents\Projetos\Blazor-WASM\Blazor_WebAssembly1\Shared\Tarefas.razor"
        
 
 
